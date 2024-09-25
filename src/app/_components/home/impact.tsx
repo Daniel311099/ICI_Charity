bad7d5747d7858e93fe0a724592f6f8622cd1cec
Daniel311099 <danielfisaha@gmail.com>
2024-09-25 22:19:37 +0100

    latest changes

--- src/app/_components/home/impact.tsx
+++ src/app/_components/home/impact.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { api } from "~/trpc/react";
 import AdvocacyAwarenessSymbol from "~/assets/graphics/Advocacy_Awareness_symbol.png";
-import CommunityEngagementSymbol from "~/assets/graphics/Community_Engagement_symbol.png";
+import CommunityEngagementSymbol from "~/assets/graphics/Community_Engagement_symbole.png";
 import EducationTrainingSymbol from "~/assets/graphics/Education_Training_symbol.png";
 import SupportServicesSymbol from "~/assets/graphics/Support_Services_symbol.png";
 import MentorshipSymbol from "~/assets/graphics/mentorship_symbol.png";
